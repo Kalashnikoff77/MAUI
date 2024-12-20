ad515e187a7b0a1d26b8e4c9336f78b1aa61b7f4
kalashnikoff77 <79265352331@yandex.ru>
2024-12-20 14:53:12 +0300

    Добавлено общение.

---
+++ Shared/Components/Pages/Messages/Messages.razor.cs
@@ -0,0 +1,65 @@
+﻿using Data.Dto.Requests;
+using Data.Dto.Responses;
+using Data.Dto.Sp;
+using Data.Services;
+using Data.State;
+using Microsoft.AspNetCore.Components;
+using Shared.Components.Dialogs;
+
+namespace Shared.Components.Pages.Messages
+{
+    public partial class Messages : IDisposable
+    {
+        [CascadingParameter] public CurrentState CurrentState { get; set; } = null!;
+        [Inject] IRepository<GetLastMessagesListRequestDto, GetLastMessagesListResponseDto> _repoGetLastMessagesList { get; set; } = null!;
+        [Inject] IRepository<MarkMessageAsReadRequestDto, MarkMessageAsReadResponseDto> _markMessageAsRead { get; set; } = null!;
+        [Inject] ShowDialogs ShowDialogs { get; set; } = null!;
+
+        List<LastMessagesForAccountSpDto> LastMessagesList = new List<LastMessagesForAccountSpDto>();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (CurrentState.Account != null)
+            {
+                var request = new GetLastMessagesListRequestDto() { Token = CurrentState.Account.Token };
+                var apiResponse = await _repoGetLastMessagesList.HttpPostAsync(request);
+                LastMessagesList = apiResponse.Response.LastMessagesList ?? new List<LastMessagesForAccountSpDto>();
+            }
+        }
+
+        async Task MarkAsReadCallback(int markAsReadId)
+        {
+            var index = LastMessagesList.FindIndex(x => x.Id == markAsReadId);
+            // Проверим, помечено ли сообщение как прочитанное и адресовано ли нам?
+            if (index >= 0 && LastMessagesList[index].Recipient?.Id == CurrentState.Account?.Id && LastMessagesList[index].ReadDate == null)
+            {
+                var apiResponse = await _markMessageAsRead.HttpPostAsync(new MarkMessageAsReadRequestDto { MessageId = markAsReadId, Token = CurrentState.Account?.Token });
+                if (apiResponse.Response.UpdatedMessage != null)
+                    LastMessagesList[index] = apiResponse.Response.UpdatedMessage;
+            }
+        }
+
+        async Task MarkAllAsReadAsync(int markAsReadId)
+        {
+            var index = LastMessagesList.FindIndex(x => x.Id == markAsReadId);
+            if (index >= 0 && LastMessagesList[index].Sender != null)
+            {
+                var apiResponse = await _markMessageAsRead.HttpPostAsync(new MarkMessageAsReadRequestDto { MessageId = markAsReadId, SenderId = LastMessagesList[index].Sender!.Id, Token = CurrentState.Account?.Token });
+                if (apiResponse.Response.UpdatedMessage != null)
+                    LastMessagesList[index] = apiResponse.Response.UpdatedMessage;
+            }
+        }
+
+        async Task BlockAccountAsync()
+        {
+        }
+
+        async Task OnSearch(string text)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
