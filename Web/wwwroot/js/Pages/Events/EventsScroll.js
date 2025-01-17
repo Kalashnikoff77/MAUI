3749353683c09af9414715c2f6d42938dbe82d21
kalashnikoff77 <79265352331@yandex.ru>
2025-01-17 15:47:43 +0300

    Добавил формулу расчёта положения скролла для дозагрузки элементов.

--- Web/wwwroot/js/Pages/Events/EventsScroll.js
+++ Web/wwwroot/js/Pages/Events/EventsScroll.js
@@ -4,23 +4,19 @@ export async function SetScrollEvent(div, dotNetReference) {
     _dotNetReference = dotNetReference; // Сохраним ссылку на C#
     var result = await _dotNetReference.invokeMethodAsync('GetNextSchedules'); // Получим сообщения
     $('#ScrollItems').append(result); // Добавим полученные сообщения в окно
-    $('#' + div).on('scroll', ScrollEvent); // Установим обработчик события прокрутки
+    $(window).on('scroll', ScrollEvent); // Установим обработчик события прокрутки
 }
 
 // Обработчик события прокрутки
 async function ScrollEvent(event) {
     var div = event.target.id; // Получим id блока
 
-    //console.log('Height: ' + h + ' - FromTop: ' + $('#' + div).scrollTop());
-    //console.log($('#Scroll').scrollTop() + '-' + $('#ScrollItems').height());
-
-    //console.log($('#ScrollItems').height() + ' - ' + $('#Scroll').scrollTop() + ' = ' + ($('#ScrollItems').height() - $('#Scroll').scrollTop()));
-
+    var winHeight = $(window).height();
+    var scrollHeight = $('#Scroll').height();
     var scrollTop = $(window).scrollTop();
-    var scrollBottom = $('#' + div).height() - $(window).height() - scrollTop;
-    console.log($('#ScrollItems').height() + ' - ' + $('#Scroll').scrollTop());
+    var result = winHeight - (scrollHeight - scrollTop) - 64 - 48;
 
-    if (($('#ScrollItems').outerHeight() - $('#Scroll').scrollTop()) < 500) {
+    if (result > -1000) {
     //if (document.getElementById(div).scrollTop + document.getElementById(div).height < 250) {
 //        $('#' + div).off('scroll'); // Временно отключим обработчик
 //        var result = await _dotNetObject.invokeMethodAsync('GetNextSchedules'); // Получим новые сообщения
