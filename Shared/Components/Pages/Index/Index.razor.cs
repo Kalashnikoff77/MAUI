d3c3978ded036b8d62641f30f7573709c6d82c3e
Олег Калашников <adm@rfpro.ru>
2025-03-04 18:40:45 +0300

    Доделаны последние участники на главной.

--- Shared/Components/Pages/Index/Index.razor.cs
+++ Shared/Components/Pages/Index/Index.razor.cs
@@ -2,18 +2,39 @@
 using Data.Dto.Responses;
 using Data.Dto.Views;
 using Data.Enums;
+using Data.Models.SignalR;
 using Data.Services;
+using Data.State;
 using Microsoft.AspNetCore.Components;
+using Shared.Components.Dialogs;
 
 namespace Shared.Components.Pages.Index
 {
     public partial class Index
     {
+        [CascadingParameter] public CurrentState CurrentState { get; set; } = null!;
         [Inject] IRepository<GetAccountsRequestDto, GetAccountsResponseDto> _repoGetAccounts { get; set; } = null!;
+        [Inject] ShowDialogs ShowDialogs { get; set; } = null!;
 
         List<AccountsViewDto> Accounts = null!;
+        IDisposable? OnMessagesUpdatedHandler;
 
-        protected override async Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync() =>
+            await GetAccounts();
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (!firstRender)
+            {
+                OnMessagesUpdatedHandler = OnMessagesUpdatedHandler.SignalRClient<OnMessagesUpdatedResponse>(CurrentState, async (response) =>
+                {
+                    await GetAccounts();
+                    await InvokeAsync(StateHasChanged);
+                });
+            }
+        }
+
+        async Task GetAccounts()
         {
             var request = new GetAccountsRequestDto
             {
@@ -26,5 +47,7 @@ namespace Shared.Components.Pages.Index
             Accounts = response.Response.Accounts;
         }
 
+        public void Dispose() =>
+            OnMessagesUpdatedHandler?.Dispose();
     }
 }
---
+++ Shared/Components/Pages/Index/OneAccount.razor.cs
@@ -0,0 +1,29 @@
+﻿using Data.Dto.Views;
+using Data.Enums;
+using Data.Extensions;
+using Data.Models;
+using Data.State;
+using Microsoft.AspNetCore.Components;
+using Shared.Components.Dialogs;
+
+namespace Shared.Components.Pages.Index
+{
+    public partial class OneAccount
+    {
+        [CascadingParameter] CurrentState CurrentState { get; set; } = null!;
+        [Parameter, EditorRequired] public ShowDialogs ShowDialogs { get; set; } = null!;
+        [Parameter, EditorRequired] public AccountsViewDto Account { get; set; } = null!;
+
+        RelationInfo? IsBlockedUser;
+        RelationInfo? HasFriendship;
+
+        protected override void OnParametersSet()
+        {
+            if (CurrentState.Account != null)
+            {
+                IsBlockedUser = CurrentState.Account.Relations?.GetRelationInfo(EnumRelations.Blocked, CurrentState.Account, Account);
+                HasFriendship = CurrentState.Account.Relations?.GetRelationInfo(EnumRelations.Friend, CurrentState.Account, Account);
+            }
+        }
+    }
+}
