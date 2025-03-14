4188f018aebfe04435cbf37e0b25231ebadc71ce
okalashnikov <okalashnikov@corp.local>
2025-03-14 14:34:14 +0300

    Технический коммит.

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,6 @@
 using Data.Services;
 using Data.State;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
-using Microsoft.AspNetCore.DataProtection;
 using MudBlazor.Services;
 using SH.Web.Services;
 using Shared.Components.Dialogs;
@@ -11,8 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel((context, options) =>
 {
-    options.Listen(System.Net.IPAddress.Any, 8000, options => options.UseHttps(@"C:\Projects\Projects\MAUI\More\certificate.pfx", "Oleg184977"));
-    options.Listen(System.Net.IPAddress.Any, 8001);
+    options.Listen(System.Net.IPAddress.Any, 443, options => options.UseHttps(@"C:\Projects\Projects\MAUI\More\certificate.pfx", "Oleg184977"));
+    options.Listen(System.Net.IPAddress.Any, 80);
 });
 
 builder.Services.AddRazorComponents()
@@ -43,7 +42,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
